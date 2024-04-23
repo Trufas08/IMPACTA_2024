4b06cc5ac76784db10dd7fb5e3725f05fed21b1f
Trufas08 <100756352+Trufas08@users.noreply.github.com>
2024-04-23 01:25:14 -0300

    segunda sprint

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, session
 from models.database import Database
 
 app = Flask(__name__)
@@ -6,17 +6,19 @@ app.secret_key = 'SENHA-MUITO-SECRETA'
 
 db = Database()
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
 
 @app.route('/cadastro', methods=["GET", "POST"])
 def cadastro():
     msg = ''
     if request.method == 'POST':
-        usuario = request.form['usuario']
-        senha = request.form['senha']
-        email = request.form['email']
+        usuario = request.form['usuario'].lower().replace(' ', '')
+        senha = request.form['senha'].replace(' ', '')
+        email = request.form['email'].lower().replace(' ', '')
 
         if usuario and senha and email:
             if not db.email_existe(email) and not db.usuario_existe(usuario):
@@ -32,6 +34,39 @@ def cadastro():
     return render_template('cadastro.html', mensagem=msg)
 
 
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    msg = ''
+    if request.method == 'POST':
+        usuario = request.form['usuario'].lower().replace(' ', '')
+        senha = request.form['senha']
+
+        if usuario and senha:
+            if db.login(usuario, senha):
+                session['usuario'] = usuario
+                return redirect('/usuario')
+            else:
+                msg = "Usuário ou senha incorretos."
+
+    return render_template('login.html', mensagem=msg)
+
+
+@app.route('/logout', methods=["POST"])
+def logout():
+    session.pop('usuario', None)
+    return redirect('/login')
+
+
+@app.route('/usuario')
+def usuario():
+    if 'usuario' in session:
+        usuario = session['usuario']
+        favoritos = db.obter_numero_favoritos(usuario)
+        avaliacoes = db.obter_numero_avaliacoes(usuario)
+        return render_template('usuario.html', usuario=usuario.capitalize(), favoritos=favoritos , avaliacoes=avaliacoes)
+    else:
+        return redirect('/login')
+
+
 if __name__ == '__main__':
     app.run()
-
--- models/database.py
+++ models/database.py
@@ -27,13 +27,47 @@ class Database:
 
 
     def cadastrar_usuario(self, usuario, email, senha):
-        hashed_senha = bcrypt.hashpw(senha.encode('utf-8'), bcrypt.gensalt())
+        hashed_senha = self.gerar_hash_senha(senha)
         comando = "INSERT INTO usuarios (nome, email, senha) VALUES (%s, %s, %s)"
         dados = (usuario, email, hashed_senha)
         self.cursor.execute(comando, dados)
         self.conexao.commit()
 
 
+    def verificar_senha(self, senha_digitada, hash_senha):
+        return bcrypt.checkpw(senha_digitada.encode('utf-8'), hash_senha.encode('utf-8'))
+    
+
+    def gerar_hash_senha(self, senha):
+        return bcrypt.hashpw(senha.encode('utf-8'), bcrypt.gensalt())
+
+
+    def login(self, usuario, senha_digitada):
+        comando = "SELECT senha FROM usuarios WHERE nome = %s"
+        self.cursor.execute(comando, (usuario,))
+        resultado = self.cursor.fetchone()
+        
+        if resultado:
+            hash_senha = resultado[0]
+            return self.verificar_senha(senha_digitada, hash_senha)
+        else:
+            return False
+
+
+    def obter_numero_favoritos(self, usuario):
+        comando = "SELECT COUNT(*) FROM Favoritos WHERE usuario_id = (SELECT usuario_id FROM Usuarios WHERE nome = %s)"
+        self.cursor.execute(comando, (usuario,))
+        resultado = self.cursor.fetchone()
+        return resultado[0] if resultado else 0
+
+
+    def obter_numero_avaliacoes(self, usuario):
+        comando = "SELECT COUNT(*) FROM Avaliacoes WHERE usuario_id = (SELECT usuario_id FROM Usuarios WHERE nome = %s)"
+        self.cursor.execute(comando, (usuario,))
+        resultado = self.cursor.fetchone()
+        return resultado[0] if resultado else 0
+    
+    
     def fechar_conexao(self):
         self.cursor.close()
         self.conexao.close()
